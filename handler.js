eafd98fa2d69c31a8b3b7ce1d6562721c7a34f3b
Sniper Girl <thilini@gmail.com>
2013-06-02 08:02:02 +1000

    Added demo.htm, support for alphabetical filter

--- handler.js
+++ handler.js
@@ -1,7 +1,9 @@
 $("document").ready(function(){
+
+//Click a tag to hide all comic pages except those that contain the tag
 	$(".tag").on('click',function(){
-		var classname = "."+$(this).attr("class").split(" ")[1];
-		var alltheseones = $("span").filter(classname);
+		var classname = "."+$(this).attr("class").split(" ")[1]
+		var alltheseones = $("span").filter(classname)
 		if ($(this).hasClass("highlighted")){
 			$(".comicpage").removeClass('hidden')
 			alltheseones.removeClass("highlighted")
@@ -15,8 +17,36 @@ $("document").ready(function(){
 		}
 		
 	})
+
+//Click "show all" to show all comic pages and tags
 	$('.all').on('click',function(){
 		$(".tag").removeClass("highlighted")
+		$(".alpha").removeClass("highlighted")
 		$(".comicpage").removeClass('hidden')
+		$(".tags").removeClass('hidden')
+	})
+
+//Show only letters that exist
+	$(".alpha").each(function(){
+		var alphabet = $(this).attr("class").split(" ")[1]
+		var alltheseones = $("#jscloud").find("span[class^='"+alphabet+"'],span[class*=' "+alphabet+"']")
+		if (alltheseones.length < 2) {
+			$(this).remove()
+		}
+	})
+	
+//Click on letter of alphabet to show only tags starting with that letter	
+	$(".alpha").on("click",function(){
+		var alphabet = $(this).attr("class").split(" ")[1]
+		if (alphabet=="num")
+		{
+		}
+		else {
+			var alltheseones = $("#jscloud").find("span[class^='"+alphabet+"'],span[class*=' "+alphabet+"']")
+			$(".alpha").removeClass("highlighted")
+			$(this).addClass("highlighted")
+			$("#jscloud").find(".tags").addClass('hidden')
+			alltheseones.closest(".tags").removeClass('hidden')		
+		}
 	})
 })
