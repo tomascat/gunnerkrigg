a92dc66648933d9d7623f948cff053e2a9bde597
Sniper Girl <thilini@gmail.com>
2013-06-19 03:59:56 +1000

    taghelper: tooltips with tag def when hover tag

--- taghelper.py
+++ taghelper.py
@@ -4,60 +4,82 @@
 import string
 import collections
 
+#initialise variables
 toWrite = []
 tagCloud = collections.Counter()
 pageDescriptions = {}
+tagDescriptions = {}
 numberOfComics = 1211
 
+#add the heading html
 with open('headingtaghelper.txt','r') as fheading:
 	for line in fheading:
 		toWrite.append(line)
 
+#populate the comic page descriptions from the last entry in index.txt
 with open('index.txt','r') as findex:
 	for line in findex:
 		field = line.split("\t")
-		pageDescriptions[int(field[0])] = field[-1][1:]
+		pageDescriptions[int(field[0])] = field[-1][1:-1]
 
+#create the dropdown comic selection with the comic number & description
 for x in reversed(xrange (1,numberOfComics)):
 	toWrite.append("\n\t\t<option value='"+str(x)+"'>"+str(x))
 	if (x in pageDescriptions):
 		toWrite.append(": "+pageDescriptions[x])
 	toWrite.append("</option>")
 
+#create the text input and 'copy to clipboard' button, the prev/next buttons, the comic div and the start of the tag section
 toWrite.append("</select><br /><br />\n\t\t\t Description: <input type='text' id='tocopy'><input type='submit' class='copyit' value='Copy To Clipboard' data-clipboard-target='tocopy'>\n\t\t</div>\n\t\t<div id='comics'>\n\t\t\t<span class='previous'><< Previous</span> <span class='next'>Next >></span>\n\t\t\t<div class='comic'></div>\n\t\t</div>\n\t\t<div id='tags'>\n\t\t\t<div id='current'>")
 
+#populate the tag dictionary
+with open("alltags.txt","r") as ftags:
+	for line in ftags:
+		field = line.split("\t")
+		tagDescriptions[field[0]]=field[1]
+
+#populate the tagcloud counter and output the 'current comic tags' section
 with open('index.txt','r') as findex:
 	for line in findex:
 		field = line.split("\t")
 		tagCloud.update(field[1:-1])
 		for i in range(1,len(field)-1):
 			tagItem = field[i].lower()
-			toWrite.append("\n\t\t\t<span class='tag hidden "+tagItem+" "+field[0]+"'>"+field[i]+"</span>")
+			toWrite.append("\n\t\t\t<span class='tag hidden "+tagItem+" "+field[0]+"'")
+			if field[i] in tagDescriptions:
+				toWrite.append(" title='"+tagDescriptions[field[i]][:-1]+"'")
+			toWrite.append(">"+field[i]+"</span>")
 
-toWrite.append("</div><div id='cloud'>")
+#end the 'current comic tags' and start the tag list section, starting with the recent tags section
+toWrite.append("\n\t\t</div>\n\t\t<div id='cloud'>\n\t\t\t<div class='recent'></div>")
 
+#output the 30 most common tags
 for (key,counter) in tagCloud.most_common(30):
 	print key + str(counter)
-	toWrite.append("\n\t\t\t<span class='tagcloud "+key+"'>"+key+"("+str(counter)+") </span> ")
-
+	toWrite.append("\n\t\t\t<span class='tagcloud "+key+"'")
+	if key in tagDescriptions:
+		toWrite.append(" title='"+tagDescriptions[key][:-1]+"'")
+	toWrite.append(">"+key+"("+str(counter)+") </span> ")
 toWrite.append("<br />")
 
+#initialise the alphabetical start character variable for the loop; output alphabetically sorted tags
 startCharOld = ""
-
 for tag in sorted(tagCloud):
 	if tag:
 		if ord(tag.upper()[0]) != startCharOld:
 			toWrite.append("<br />"+tag.upper()[0]+": ")
-		toWrite.append("\n\t\t\t<span class='tagcloud "+tag+"'>"+tag+"("+str(tagCloud[tag])+") </span> ")
+		toWrite.append("\n\t\t\t<span class='tagcloud "+tag+"'")
+		if tag in tagDescriptions:
+			toWrite.append(" title='"+tagDescriptions[tag][:-1]+"'")
+		toWrite.append(">"+tag+"("+str(tagCloud[tag])+") </span> ")
 		startCharOld = ord(tag.upper()[0])
 
-toWrite.append("")
-	
+#add the ending html
 with open('endingtaghelper.txt','r') as fending:
 	for line in fending:
 		toWrite.append(line)
 
+#convert the toWrite array to a string and write that string to taghelper.htm
 s = ''.join(toWrite)
-
 with open("taghelper.htm","w") as ftagged:
 	ftagged.write(s)
